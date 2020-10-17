0d9c671a3aa4b4c55ab9be0cabb7cc12fee11d22
unknown <Atikaindahhh18@gmail.com>
2020-10-17 15:41:54 +0700

    Bia Has Been Commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,19 @@
+<?php namespace App\Controllers;
+
+class Home extends BaseController
+{
+	public function index()
+	{ 
+		return view('welcome_message');
+	}
+
+	//--------------------------------------------------------------------
+	public function show()
+	{
+		$data ['nama']= 'bia';
+		$data ['jurusan']= 'ilkomp';
+		echo view('mahasiswa/header', $data);
+		echo view('mahasiswa/index', $data); 
+		echo view('mahasiswa/footer', $data);
+	}
+}
